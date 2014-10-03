ebc53eccee0472c9130fa2e45ce16629e2f3a130
linus1412 <linus141275@hotmail.com>
2014-10-03 10:39:52 +0100

    Create Application.java

---
+++ src/main/java/smitek/Application.java
@@ -0,0 +1,24 @@
+package smitek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+        EventRepository repository = context.getBean(EventRepository.class);
+
+        // save a couple of customers
+        repository.save(new Event("Christmas"));
+        repository.save(new Event("Olly's Birthday"));
+        repository.save(new Event("Tuesday"));
+        repository.save(new Event("Zach's first word"));
+        repository.save(new Event("Pancake Day"));
+    }
+
+}
